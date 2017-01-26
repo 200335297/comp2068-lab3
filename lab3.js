e3ea2db6ddd07cb27bffa5c95193a41e798e8e83
200335297 <Gowtham.Talluri@MyGeorgian.ca>
2017-01-26 11:40:38 -0500

    Add files via upload

---
+++ lab3.js
@@ -0,0 +1,69 @@
+// lab3.js
+
+// bring connect into our file
+var connect = require('connect');
+
+// load in the url module to parse url parameters
+var url = require('url');
+
+// filesystem module
+var fs = require('fs');
+
+// instantiate our app from connect
+var app = connect();
+
+// Calculator function
+function Calculator(req, res){
+     //collect the parameters [method,x,y] from url
+    // http://localhost:3000/lab3.js?method=add&x=16&y=4
+    var parameters = url.parse(req.url, true).query;
+    var x = parameters.x;
+    var y = parameters.y;
+    var methodname = parameters.method ;
+    var result;
+    var symbol;
+
+    switch(methodname){
+        case "add":
+            result=parseFloat(x)+parseFloat(y);
+            symbol = "+";
+            break;
+        case "subtract":
+            result=parseFloat(x)-parseFloat(y);
+            symbol = "-";
+            break;
+        case "multiply":
+            result=parseFloat(x)*parseFloat(y);
+            symbol = "*";
+            break;
+        case "divide":
+            result= (parseFloat(x))/(parseFloat(y));
+            symbol = "/";
+            break;
+        default:
+            symbol ='';
+            result= "Enter correct method name";
+    }
+
+
+    // display result value
+    if(symbol!='') {
+        res.end("output: " + x + " " + symbol + " " + y + " = " + String(result));
+    }else{
+        res.end("Error: " + String(result));
+    }
+}
+
+app.use('/lab3.js', Calculator)
+
+// start the server
+// port 3000
+app.listen(3000)
+
+// spit out to the console telling us
+// that the server is running
+console.log('Connect running on port 3000')
+
+/**
+ * Created by Gowtham on 2017-01-25.
+ */
